45ac8355bed99992890914e99a0c4884caee4a65
David Góngora <fdgongora@iesmurgi.org>
2020-10-05 21:21:29 +0200

    Agregar archivos de proyecto.

---
+++ AppXamarinMov_Calculadora/AppXamarinMov_Calculadora/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppXamarinMov_Calculadora
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void Button_Numeros_Clicked(object sender, EventArgs e)
+        {
+            Visor.Text += ((Button)sender).Text;
+        }
+
+        private void btn_Resultado_Clicked(object sender, EventArgs e)
+        {
+            St_OperacionesAnteriores.Children.Add(new Label() { Text = Visor.Text, FlowDirection = FlowDirection.RightToLeft }) ;
+            var lastChild = St_OperacionesAnteriores.Children.LastOrDefault();
+            if (lastChild != null)
+                Sv_historial.ScrollToAsync(lastChild, ScrollToPosition.MakeVisible, true);
+
+        }
+    }
+}
